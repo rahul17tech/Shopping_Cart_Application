65497dce2176ca4ee23a22480863ec1bdb79631e
rahul17tech <rahulchakraborty696@gmail.com>
2024-10-07 22:02:31 +0530

    First Commit
    07-10-2024

---
+++ shoppingkartusingcollection/src/shoppingkartusingcollection/ShoppingKartUsingArrayList.java
@@ -0,0 +1,11 @@
+package shoppingkartusingcollection;
+
+public class ShoppingKartUsingArrayList {
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("\t\t~~ Welcome To Shopping Kart ~~");
+		ShoppingKartArrayListOperation.mainMenu();
+	}
+
+}
---
+++ shoppingkartusingcollection/src/shoppingkartusingcollection/exception/GlobalExceptionForProduct.java
@@ -0,0 +1,10 @@
+package shoppingkartusingcollection.exception;
+
+public class GlobalExceptionForProduct extends Exception {
+	
+	public GlobalExceptionForProduct (String message)
+	{
+		super(message);
+	}
+
+}
